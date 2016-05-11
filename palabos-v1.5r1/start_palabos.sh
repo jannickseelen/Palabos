e6a7adf337eebd07777aedfe4b4d97aca59f1151
Jannick Seelen <j.seelen@student.tudelft.nl>
2016-05-11 12:50:01 +0200

    Updated batch file for CentOS

--- palabos-v1.5r1/start_palabos.sh
+++ palabos-v1.5r1/start_palabos.sh
@@ -2,15 +2,42 @@
 #PBS -N PALABOS 
 #PBS -m abe
 #PBS -M jannick.seelen@gmail.com
-#PBS -l nodes=1:ppn=48
+#PBS -l nodes=2:ppn=48
 #PBS -q rrr-pnr
 #PBS -o "~/Palabos/run.log"
 #PBS -j oe
 
-cd ~/Palabos/palabos-v1.5r1/build
+set -e
 
-make clean
+cd ~/Palabos/palabos-v1.5r1/build/
 
-make
+echo "Removing previous build"
+
+rm -rf cmake*
+rm -rf CMake*
+rm -rf *.cmake
+rm -rf *.ninja
+rm -rf ninja*
+rm -rf *.dSYM
+rm -rf src
+rm -rf viscosityTest
+rm -rf *.dylib
+rm -rf dlib*
+
+echo "Loading mpi module"
+
+module load "/etc/modulefiles/openmpi-x86_64"
+
+echo "Running CMake"
+
+cmake -v ~/Palabos/palabos-v1.5r1
+
+echo "Running make"
+
+make -j48
+
+echo "Running MPI"
+
+chmod +x viscosityTest
 
 mpirun ./viscosityTest /input/parameters.xml
