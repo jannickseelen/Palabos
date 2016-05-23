5880890cbea556f8f089de767bcd12eb2063ee30
Jannick <j.seelen@student.tudelft.nl>
2016-05-23 16:32:22 +0200

    Moved exceptions and stacktrace to header files

---
+++ palabos-v1.5r1/viscosityTest/src/backtrace.hh
@@ -0,0 +1,24 @@
+#ifndef BACKTRACE_HH
+#define BACKTRACE_HH
+
+// C++ INCLUDES
+#include <execinfo.h> // For complete stacktrace on exception see GNU libc manual
+#include <iostream>
+
+namespace plb{
+	void printTrace (void){			// Obtain a backtrace and print it to stdout.
+	try{
+		void *Array[10];
+		int size;
+		char **strings;
+		int i;
+		size = backtrace (Array, 10);
+		strings = backtrace_symbols (Array, size);
+		std::cout << "Obtained "<< size << "stack frames" << std::endl;
+		for (i = 0; i < size; i++){printf ("%s\n", strings[i]);}
+		free (strings);
+	}
+	catch(std::exception& e){ std::cout << "Exception Caught: " << e.what() << "\n"; throw; }
+	}
+}
+#endif //BACKTRACE_HH
