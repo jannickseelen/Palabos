1d472d18a3348a0975c67e67b26a632f04d931a8
Jannick <j.seelen@student.tudelft.nl>
2016-08-30 15:35:16 +0200

    typo again

--- src/variables.hh
+++ src/variables.hh
@@ -216,7 +216,7 @@ namespace plb{
 					voxelizedDomain.getMultiBlockManagement(),
 					voxelizedDomain.getBlockCommunicator(),
 					voxelizedDomain.getCombinedStatistics(),
-					DefaultMultiBlockPolicy3D::getMultiCellAcces(),
+					DefaultMultiBlockPolicy3D::getMultiCellAccess(),
 					new IncBGKdynamics<T,Descriptor>(p.getOmega())
 					)
 				);
